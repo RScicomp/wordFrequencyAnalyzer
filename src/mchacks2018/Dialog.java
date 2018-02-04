a7e7c673f6c17cc610fd7a64940303a02745f9e5
Yutong Zhang <32484947+yutongz1997@users.noreply.github.com>
2018-02-04 11:22:26 -0500

    Updated

--- src/mchacks2018/Dialog.java
+++ src/mchacks2018/Dialog.java
@@ -3,29 +3,55 @@ package mchacks2018;
 import org.jfree.ui.RefineryUtilities;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.filechooser.FileSystemView;
 import java.awt.*;
+import java.io.File;
 
 public class Dialog extends JDialog {
     private JLabel labelPath;
-    private JTextField filePathField;
-    private JButton generateChart;
+    private JTextField fieldFilePath;
+    private JButton buttonGenerateChart;
+    private JButton buttonChooseFile;
 
     public Dialog() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException
+                | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+
         labelPath = new JLabel("PDF Path: ");
-        filePathField = new JTextField();
-        generateChart = new JButton("Generate PDF");
+        fieldFilePath = new JTextField();
+        buttonGenerateChart = new JButton("Generate Chart");
+        buttonChooseFile = new JButton("...");
 
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         JPanel panelPath = new JPanel();
         panelPath.setLayout(new BorderLayout());
+        panelPath.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         panelPath.add(labelPath, BorderLayout.WEST);
-        panelPath.add(filePathField, BorderLayout.CENTER);
+        panelPath.add(fieldFilePath, BorderLayout.CENTER);
+        panelPath.add(buttonChooseFile, BorderLayout.EAST);
         mainPanel.add(panelPath);
-        mainPanel.add(generateChart);
+        mainPanel.add(buttonGenerateChart);
 
-        generateChart.addActionListener((event) -> {
-            PDFReader.convertToText(filePathField.getText(), "temp.txt", 0, -1);
+        buttonChooseFile.addActionListener((event) -> {
+            JFileChooser chooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("PDF Files (*.pdf)", "pdf");
+            chooser.setFileFilter(filter);
+            int returnValue = chooser.showOpenDialog(null);
+            if (returnValue == JFileChooser.APPROVE_OPTION) {
+                File selectedFile = chooser.getSelectedFile();
+                fieldFilePath.setText(selectedFile.getAbsolutePath());
+            }
+        });
+
+        buttonGenerateChart.addActionListener((event) -> {
+            PDFReader.convertToText(fieldFilePath.getText(), "temp.txt", 0, -1);
             Parser parser = new Parser();
             parser.sortByFrequency("temp.txt", "commonWords.txt", "data.txt");
 
@@ -41,7 +67,7 @@ public class Dialog extends JDialog {
         pack();
         setLocationRelativeTo(null);
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        setTitle("wordXtract");
+        setTitle("Word Frequency Analyzer");
         setResizable(false);
     }
 }
